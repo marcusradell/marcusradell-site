4f81d9a4cb9e8d9bfbd360628921dadee94a56bc
Marcus Rådell <marcus@radell.net>
2024-12-13 21:03:09 +0100

    set tailwind content glob

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,8 @@ import tailwindTypography from "@tailwindcss/typography";
 export default {
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
