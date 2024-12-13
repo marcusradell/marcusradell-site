73bce9d053770759003d3ac2faf858f45ce84bab
Marcus Rådell <marcus@radell.net>
2024-12-13 21:37:29 +0100

    add an h2 component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { H1 } from "@/components";
+import { H1, H2 } from "@/components";
 
 export default function Home() {
   return (
     <main className="container mx-auto pt-4">
       <H1>Marcus Rådell</H1>
+      <H2>About Me</H2>
+      <p>
+        I&apos;m a software developer with a wide set of skills related to
+        develop ideas to software products in a professional team environment.
+      </p>
     </main>
   );
 }
