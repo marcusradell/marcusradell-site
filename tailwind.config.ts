f1f9cfc6b6d30955a1808e24d479c346658fee2b
Marcus Rådell <marcus@radell.net>
2024-12-13 21:28:40 +0100

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ import tailwindAnimate from "tailwindcss-animate";
 export default {
   darkMode: ["class"],
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
