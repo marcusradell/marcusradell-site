6004bc0580afd177a501f5453ccfc96b4cbcdd7e
Marcus Rådell <marcus@radell.net>
2024-12-16 22:33:04 +0100

    add the three pillars as paragraphs

--- src/components/ui/typography/index.ts
+++ src/components/ui/typography/index.ts
@@ -1,3 +1,4 @@
 export * from "./h1";
 export * from "./h2";
 export * from "./lead";
+export * from "./p";
---
+++ src/components/ui/typography/p.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+type Props = { children: ReactNode };
+
+export function P({ children }: Props) {
+  return (
+    <p className="leading-7 text-muted-foreground [&:not(:first-child)]:mt-6">
+      {children}
+    </p>
+  );
+}
