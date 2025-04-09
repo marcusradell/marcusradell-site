51c29241687ce56f17920e336332c5c92102dad5
Marcus Rådell <marcus@radell.net>
2025-04-09 18:56:34 +0200

    add some x padding to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { AboutMe } from "@/features";
 
 export default function Home() {
   return (
-    <main className="container mx-auto pt-4">
+    <main className="container mx-auto px-2 pt-4">
       <H1>Marcus Rådell</H1>
       <AboutMe />
     </main>
