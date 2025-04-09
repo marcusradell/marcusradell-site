a8d594db26c9e3bf4b2c6f122f8d7111f507fa11
Marcus Rådell <marcus@radell.net>
2025-04-09 20:01:57 +0200

    rewrite the three roles texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,23 +22,21 @@ export default function Home() {
         </div>
         <div className="md:pl-8">
           <P>
-            As a manager, I value psychological safety and approach each
-            individual with curiosity. I do not squeeze value out of my teams,
-            but take care of the value overflow that comes when developers
-            thrive at work.
+            As a manager, Marcus values psychological safety and approach each
+            individual with curiosity. He channels the natural value that comes
+            from developers that thrive at work.
           </P>
           <P>
-            As a product developer, I want to bring value to my users, so that I
-            can feel pride in my work. I tend to sew together product processes
-            that creates a symbiosis between product people and programmers.
-            Extreme programming, mob programming, test-driven developement, user
-            story mapping, and lean processes are keywords that represents me.
+            As a product developer, Marcus feels pride by bringing value to the
+            users. He merges product planning with agile development. Extreme
+            programming, mob programming, test-driven developement, user story
+            mapping, and lean processes are keywords that represents his work
+            style.
           </P>
           <P>
-            As a programmer, I believe that quality software takes less effort
-            to write than rushed software. Quality code mainly relates to
-            coupling and trust. Can we trust the code enough to change it? If
-            so, our system will be much cheaper to maintain.
+            As a programmer, Marcus believes that quality software takes less
+            effort to write than rushed software. He focuses on decoupling
+            features to keep the cost of maintenance low.
           </P>
         </div>
       </div>
